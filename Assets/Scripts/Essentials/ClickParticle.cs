2603d068579edab504e60f2f04af1a99cc5634ce
stangshineZ <stangxv@gmail.com>
2025-10-23 15:00:24 +0700

    Feat: Click/Skip Particle

---
+++ Assets/Scripts/Essentials/ClickParticle.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickParticle : MonoBehaviour
+{
+    [SerializeField] ParticleSystem clickParticle;
+    [SerializeField] ParticleSystem spaceParticle;
+
+    void Update()
+    {
+        Vector3 mouseScreenPosition = Input.mousePosition;
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mouseScreenPosition);
+        Vector2 mouseWorldPosition2D = new Vector2(mouseWorldPosition.x, mouseWorldPosition.y);
+
+        if(Input.GetKeyDown(KeyCode.Mouse0)) Instantiate(clickParticle, mouseWorldPosition2D, Quaternion.identity);
+        if(Input.GetKeyDown(KeyCode.Space) && spaceParticle) spaceParticle.Play();
+    }
+}
