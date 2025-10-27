b7549ffdcdc0cb17501d0fc91eb6c6fc30b5713b
ikqzaa123 <ikqzaa.phurinat@gmail.com>
2025-10-27 18:11:52 +0700

    Room 3  PuzzleUI + Dialogue + Camerapanning
    Camerapanningabletostopwhendialogue + MainGame Scene

--- Assets/Scripts/Room3.cs
+++ Assets/Scripts/Room3.cs
@@ -1,36 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Room3 : MonoBehaviour
 {
     public bool isRikaCalm = false;
     public int clickCount = 0;
-  
+    [SerializeField] private Button calmButton;
+    [SerializeField] private GameObject spamText;
 
     void Start()
     {
-        
+        calmButton.gameObject.SetActive(false);
+        spamText.gameObject.SetActive(false);
     }
-
 
     void Update()
     {
         if (clickCount == 30)
         {
-            Destroy(gameObject);
             isRikaCalm = true;
+            calmButton.gameObject.SetActive(false);
+            spamText.gameObject.SetActive(false);
         }
     }
 
-    void OnMouseDown()
+    public void OnButtonClick()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            clickCount++;
-        }
+        clickCount++;
     }
-
 
 }
