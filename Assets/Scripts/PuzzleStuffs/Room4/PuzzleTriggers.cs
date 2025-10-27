3466bd987e8f6ba7e72939e0e403fceab126b605
GolfWasFound <tanawatgolf2003@gmail.com>
2025-10-27 19:30:19 +0700

    Puzzle 1 and 2 Gameplay mostly completed
    Puzzle 1 Implementation:
    - 7 buttons corresponding to each day of the week.
    - Upon pressing a date button, a red circle will appear and that date can no longer be chosen again.
    - Upon clicking the correct button, every unclicked button will no longer be clicked again.
    
    Puzzle 2 Implementation:
    - Basically StangShineZ's code, but with only 1 target position.
    
    Puzzle 3:
    - Clock [Done 80%]
    - Lock [50%]

--- Assets/Scripts/PuzzleStuffs/Room4/PuzzleTriggers.cs
+++ Assets/Scripts/PuzzleStuffs/Room4/PuzzleTriggers.cs
@@ -1,32 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PuzzleTriggers : MonoBehaviour
 {
-    [SerializeField] Clock clock;
     [SerializeField] DialogueMaker screech;
-    [SerializeField] DialogueMaker puzzle1failure;
-    [SerializeField] DialogueMaker puzzle1solved;
+    [SerializeField] DialogueMaker puzzle1Failure;
+    [SerializeField] DialogueMaker puzzle1Solved;
+    [SerializeField] GameObject[] scribbles;
+    [SerializeField] GameObject[] dateButtons;
+    [SerializeField] GameObject circleMarker;
+    [SerializeField] Clock clock;
+
+    public int failcount;
+
+    void Awake()
+    {
+        failcount = 0;
+    }
 
     public void Puzzle1Wrong()
     {
-        Debug.Log("Wrong");
-        screech.StartDialogue();
-        if (true) // TBA
+        if (!circleMarker.activeSelf) circleMarker.SetActive(true);
+        failcount++;
+        Debug.Log("Wrong \n Fail Count : " + failcount);
+        if (failcount > 0 && failcount <= 6)
         {
-            puzzle1failure.StartDialogue();
+            // Plays screeching noise.
+            screech.StartDialogue();
+            // Shows scribbling for each failure.
+            scribbles[failcount - 1].SetActive(!scribbles[failcount - 1].activeSelf);
+            circleMarker.transform.position = dateButtons[failcount - 1].transform.position;
+            if (failcount == 3)
+            {
+                // Two girls yap about their predicament.
+                puzzle1Failure.StartDialogue();
+            }
         }
     }
+    
     public void Puzzle1Correct()
     {
+        if (!circleMarker.activeSelf) circleMarker.SetActive(true);
         Debug.Log("Correct");
-        puzzle1solved.StartDialogue();
+        // Deactivates every unused butttons.
+        foreach (GameObject button in dateButtons)
+        {
+            if (button.activeSelf)
+            {
+                button.SetActive(false);
+            }
+        }
+        circleMarker.transform.position = dateButtons[dateButtons.Length - 1].transform.position;
+        puzzle1Solved.StartDialogue();
     }
     
     public void TriggerPuzzle3()
     {
-        clock.StopAtTime(9, 35);
+        clock.StopAtTime(09, 35);
         // TBA
     }
 }
